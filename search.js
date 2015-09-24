06225872a1ca59a823dd484fc474b8eafbee3549
Hung-Han Chen <chenhungh@gmail.com>
2015-09-24 23:07:50 +0300

    sss

--- search.js
+++ search.js
@@ -1,18 +1,39 @@
-function handleAPILoaded() {
-    $('#search-button').attr('disabled', false);
-    console.log('disabled')
+// Your use of the YouTube API must comply with the Terms of Service:
+// https://developers.google.com/youtube/terms
+
+// Helper function to display JavaScript value on HTML page.
+function showResponse(response) {
+    var responseString = JSON.stringify(response, '', 2);
+    document.getElementById('response').innerHTML += responseString;
 }
 
-// Search for a specified string.
+// Called automatically when JavaScript client library is loaded.
+function onClientLoad() {
+    gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
+}
+
+// Called automatically when YouTube API interface is loaded (see line 9).
+function onYouTubeApiLoad() {
+    // This API key is intended for use only in this lesson.
+    // See https://goo.gl/PdPA1 to get a key for your own applications.
+    gapi.client.setApiKey('WtoLk3P31QDZwHYzFWI1kzXI');
+
+    search();
+}
+
 function search() {
-    var q = $('#query').val();
+    // Use the JavaScript client library to create a search.list() API call.
     var request = gapi.client.youtube.search.list({
-        q: q,
-        part: 'snippet'
+        part: 'snippet',
+
     });
 
-    request.execute(function(response) {
-        var str = JSON.stringify(response.result);
-        $('#search-container').html('<pre>' + str + '</pre>');
-    });
+    // Send the request to the API server,
+    // and invoke onSearchRepsonse() with the response.
+    request.execute(onSearchResponse);
+}
+
+// Called automatically with the response of the YouTube API request.
+function onSearchResponse(response) {
+    showResponse(response);
 }
