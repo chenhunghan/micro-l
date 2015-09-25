b4c630a0e39e57c25e149560279a514fa849c7c2
Hung-Han Chen <chenhungh@gmail.com>
2015-09-25 20:14:34 +0300

    init

---
+++ app.js
@@ -0,0 +1,34 @@
+function onClientLoad() {
+    var keyword = 'javascript'
+    gapi.client.load('youtube', 'v3', function() {
+        gapi.client.setApiKey('AIzaSyDG4OKT4eGlYFtpCmfKM8jGs42ge7qM--s');
+        var request = gapi.client.youtube.search.list({
+            q: keyword + '|step-by-step|tutorial|beginners|how -music -song',
+            part: 'snippet',
+            type: 'video',
+            safeSearch: 'moderate',
+            videoDuration: 'short',
+            videoEmbeddable: true,
+            relevanceLanguage: 'eng',
+            maxResults: 10
+            //https://developers.google.com/youtube/v3/docs/search/list#request-body
+        });
+        request.execute(function(response) {
+
+            angular.module('app', ['ngAnimate', 'ui.bootstrap'])
+            angular.module('app').controller('MainCtrl', MainCtrl)
+
+            function MainCtrl($scope) {
+                $scope.items = response.items
+            };
+
+            angular.element(document).ready(function() {
+                angular.bootstrap(document, ['app']);
+            });
+        });
+    });
+
+
+
+
+}
